2903c61dcbd64450f0166c015166e56a45dffc8d
Raif Agovic <raif.agovic.dev@gmail.com>
2024-01-20 17:40:48 +0100

    Draw the text

--- AMSGenerator.swift
+++ AMSGenerator.swift
@@ -1,32 +1,30 @@
 import Foundation
-import Cocoa
 import CoreGraphics
 import CoreServices
 
 // Set the path to the image in the Resources folder
 let imagePath = "Resources/ams_form.png"
 
-// Load image using NSImage
-guard let image = NSImage(contentsOfFile: imagePath),
-      let cgImage = image.cgImage(forProposedRect: nil, context: nil, hints: nil) else {
+// Create a mutable bitmap context from the image
+let inputURL = URL(fileURLWithPath: imagePath)
+guard let imageSource = CGImageSourceCreateWithURL(inputURL as CFURL, nil),
+      let cgImage = CGImageSourceCreateImageAtIndex(imageSource, 0, nil) else {
     // Handle error loading image
     exit(1)
 }
 
-// Create a mutable bitmap context
 let width = cgImage.width
 let height = cgImage.height
 
 let colorSpace = CGColorSpaceCreateDeviceRGB()
 let bytesPerPixel = 4
-let bytesPerRow = bytesPerPixel * width
 let bitsPerComponent = 8
 
 guard let context = CGContext(data: nil,
                               width: width,
                               height: height,
                               bitsPerComponent: bitsPerComponent,
-                              bytesPerRow: bytesPerRow,
+                              bytesPerRow: bytesPerPixel * width,
                               space: colorSpace,
                               bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue) else {
     // Handle error creating graphics context
@@ -40,20 +38,12 @@ context.draw(cgImage, in: CGRect(x: 0, y: 0, width: width, height: height))
 let text = "John Doe"
 let textCoordinates = CGPoint(x: 100, y: 200)
 
-// Use NSFont and NSColor for text attributes
-let textAttributes: [NSAttributedString.Key: Any] = [
-    .font: NSFont.systemFont(ofSize: 12),
-    .foregroundColor: NSColor.black
-]
-
-let attributedText = NSAttributedString(string: text, attributes: textAttributes)
-
-// Draw the text onto the context at the specified coordinates
-context.saveGState()
-context.translateBy(x: 0, y: CGFloat(height))
-context.scaleBy(x: 1, y: -1) // Flip the coordinate system
-attributedText.draw(at: textCoordinates)
-context.restoreGState()
+// Use Core Text for text drawing
+let textFont = CTFontCreateWithName("Helvetica" as CFString, 12, nil)
+let textString = NSAttributedString(string: text, attributes: [.font: textFont, .foregroundColor: CGColor.black] as [NSAttributedString.Key : Any])
+let textLine = CTLineCreateWithAttributedString(textString)
+context.textPosition = textCoordinates
+CTLineDraw(textLine, context)
 
 // Save the final image to the same Resources folder
 let outputURL = URL(fileURLWithPath: "Resources/output.png")
@@ -66,6 +56,7 @@ CGImageDestinationAddImage(destination, context.makeImage()!, nil)
 CGImageDestinationFinalize(destination)
 
 print("Image saved to \(outputURL.path)")
+
 
 
 
