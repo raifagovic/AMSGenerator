a1837229f0dd58f666935323b3b640b034f466b1
Raif Agovic <raif.agovic.dev@gmail.com>
2024-02-07 14:07:01 +0100

    Create a new file TaxCalculation.swift

---
+++ TaxCalculation.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+var totalCalculatedDeduction: Double = 0.0
+var totalAmountOfIncome: Double = 0.0
+var totalHealthInsurance: Double = 0.0
+var totalTaxBase: Double = 0.0
+var totalTaxCreditPaidAbroad: Double = 0.0
+var totalTaxDifferenceForPayment: Double = 0.0
+
